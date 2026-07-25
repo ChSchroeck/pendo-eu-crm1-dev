95952efc09c2e8cb08057161a291a4629d6ac5f9
ChSchroeck <47699204+ChSchroeck@users.noreply.github.com>
2026-07-25 11:38:50 +0200

    Update pendo-snippet.js
    adding matomo

--- pendo-js/pendo-snippet.js
+++ pendo-js/pendo-snippet.js
@@ -68,3 +68,18 @@
                 }
             }); */
         })('7d66e821-ff08-4795-a340-1a3ebbd3df49');
+
+
+
+
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://t.schroeck-online.de/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '2']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
